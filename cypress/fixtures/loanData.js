66f749910f70914a724b96b46f1b7b92c6dac192
JhenniferJS <jhennifer@gec.inatel.br>
2022-11-24 16:51:34 -0300

    update installment value

--- cypress/fixtures/loanData.js
+++ cypress/fixtures/loanData.js
@@ -1,5 +1,5 @@
 export const loanData = [
     {amount: 1000, installments: 6, installmentsValue: '271,27'},
     {amount: 4000, installments: 12, installmentsValue: '478,49'},
-    {amount: 6000, installments: 24, installmentsValue: '387,89'}
+    {amount: 6000, installments: 24, installmentsValue: '347,89'}
 ]
